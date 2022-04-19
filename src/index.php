9a9bb2a8f0374cf6fe44a0f6faf72f68b3c241db
Yuta0227 <yutahonjo@keio.jp>
2022-04-19 15:18:07 +0900

    トップページヘッダーhtml完成

--- src/index.php
+++ src/index.php
@@ -12,7 +12,7 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>サンプル</title>
+    <title>トップページ</title>
 </head>
 <ul>
     <?php foreach ($events as $key => $event) : ?>
