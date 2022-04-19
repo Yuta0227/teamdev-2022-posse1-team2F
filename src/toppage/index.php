4bcfca8d6dbf637bc118d7773fd8bc33daf844da
Yuta0227 <yutahonjo@keio.jp>
2022-04-19 17:01:43 +0900

    並び替えガイド絞り込みボタンhtml完成

--- src/toppage/index.php
+++ src/toppage/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require(dirname(__FILE__) . "/dbconnect.php");
+require("../dbconnect.php");
 
 $stmt = $db->query('SELECT id, title FROM events');
 $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -14,14 +14,39 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>トップページ</title>
 </head>
-<ul>
+<!-- <ul>
     <?php foreach ($events as $key => $event) : ?>
         <li>
             <?= $event["id"]; ?>:<?= $event["title"]; ?>
         </li>
     <?php endforeach; ?>
     <a href="/admin/index.php">管理者ページ</a>
-</ul>
+</ul> -->
+<section>
+    <!--並び替え、就活ガイド、絞り込み-->
+    <div>
+        <!--並び替え-->
+        <div>
+            <!--横並び-->
+            <form>
+                <div>
+                    <select>
+                        <option>並び替え条件</option>
+                        <option>名前順</option>
+                    </select>
+                </div>
+                <div>
+                    <input type="submit" value="並び替える">
+                </div>
+            </form>
+        </div>
+        <!--絞り込み-->
+        <button>絞り込み</button>
+        <!--ガイド-->
+        <button>どの条件で絞り込めばいいかわからない方はこちら!</button>
+    </div>
+    <div>検索結果：<span>10</span>/<span>30</span>件</div><!--検索結果下の方に表示した方がいいと思った。レスポンシブの時文字数きついかも。-->
+</section>
 
 <body>
 </body>
