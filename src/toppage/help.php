0fa6dcff2756c844b2047afc82802db470c9c758
Yuta0227 <yutahonjo@keio.jp>
2022-04-21 17:59:05 +0900

    html完成あとはレビュー

---
+++ src/toppage/help.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ヘルプページ</title>
+</head>
+<body>
+    <?php require "header.php";?>
+    <p>ご不明点や企業様の掲載申込の問い合わせはこちらまで</p>
+    <p>sample@gmail.com</p>
+    <?php require "footer.php";?>
+</body>
+</html>
