a91efc775365400db3ca1d30583b1f230e9ff768
Yuta0227 <yutahonjo@keio.jp>
2022-04-21 17:26:10 +0900

    特集詳細上側html完成。あとはレビュー

---
+++ src/toppage/special_detail_index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>特集詳細</title>
+</head>
+<body>
+    <?php require "header.php";?>
+    <div style="display:flex;">
+        <div>
+            <img alt="画像">
+        </div>
+        <div>
+            <h1 style="border-bottom:1px solid black;">タイトル</h1>
+        </div>
+    </div>
+    <?php require "footer.php";?>
+</body>
+</html>
