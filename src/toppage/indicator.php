ebfab1e1480fc4aa135327f64ce6a3468bfaa83f
Yuta0227 <yutahonjo@keio.jp>
2022-04-21 18:48:46 +0900

    html完成. あとはれびゅ

---
+++ src/toppage/indicator.php
@@ -0,0 +1,21 @@
+<div style="width:100vw;display:flex;background-color:white;border:1px solid black;justify-content:center;align-items:center;">
+    <div>
+        <div>STEP1</div>
+        <div style="border-radius:50%;background-color:skyblue;">記事を選ぶ</div>
+    </div>
+    <div>-&gt;</div>
+    <div>
+        <div>STEP2</div>
+        <div style="border-radius:50%;background-color:skyblue;">カートに追加</div>
+    </div>
+    <div>-&gt;</div>
+    <div>
+        <div>STEP3</div>
+        <div style="border-radius:50%;background-color:skyblue;">カート確認</div>
+    </div>
+    <div>-&gt;</div>
+    <div>
+        <div>STEP4</div>
+        <div style="border-radius:50%;background-color:skyblue;">情報入力</div>
+    </div>
+</div>
