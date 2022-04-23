c9dd1d02dd3d27b67339cfa9d3d97a470e1edcaa
Rena Yamadera <renapi200i@i.softbank.jp>
2022-04-23 16:03:10 +0900

    とりあえず見た目完成,就活ドットコムのロゴ画像黒から白に変えます

--- src/toppage/header.php
+++ src/toppage/header.php
@@ -4,18 +4,19 @@
         <img class="header-logo" src="../img/syukatudotcom_logo.png" alt="就活.com">
     </div>
     <!-- ヘッダーの下側 -->
-    <div class="header-under">
+    <div class="header-bottom-box">
         <!--ヘッダーの下側の左側-->
         <div class="header-menu">
-            <div>TOP</div>
-            <div>特集記事</div>
-            <div>ヘルプ</div>
-            <div>企業様のログインはこちら</div>
+            <div class="each-menu">TOP</div>
+            <div class="each-menu">特集記事</div>
+            <div class="each-menu">ヘルプ</div>
+            <div class="each-menu">企業様のログインはこちら</div>
         </div>
         <!--ヘッダーの下側の右側-->
         <div class="header-cart-box">
-            <div class="header-cart">カート<!--position:relative-->
-                <div class="cart-count">数字</div><!--position:absoluteでごり押し-->
+            <div class="header-cart">
+            <i class="fa-solid fa-envelope fa-2x mail-icon"></i><!--position:relative-->
+                <div class="cart-count">1</div><!--position:absoluteでごり押し-->
             </div>
         </div>
     </div>
