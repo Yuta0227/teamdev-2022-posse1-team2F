1b0e5e85b472a284b546432aeb358b3876f0f7cb
Yuta0227 <yutahonjo@keio.jp>
2022-04-25 21:57:06 +0900

    一旦コンフリクト解消

---
+++ src/admin/pages/admin_agent_detail.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理者専用企業詳細画面</title>
+</head>
+<body>
+    <?php 
+    require "../parts/header.php";
+    require "../parts/admin_agent_detail_table.php";
+    ?>
+
+</body>
+</html>
---
+++ src/admin/parts/admin_agent_detail_table.php
@@ -0,0 +1,26 @@
+<section>
+    <div style="display:flex;">
+        <img src="" alt="企業の写真">
+        <div>株式会社アンチパターン</div>
+    </div>
+    <form>
+        <table>
+
+        </table>
+        <div>
+            <button>編集</button>
+        </div>
+    </form>
+    <form>
+        <table>
+
+        </table>
+        <div>
+            <button>編集</button>
+        </div>
+    </form>
+    <form>
+        <div>エージェント説明文</div>
+        <div></div>
+    </form>
+</section>
