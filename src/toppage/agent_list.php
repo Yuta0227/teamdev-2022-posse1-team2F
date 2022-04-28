781857c58998771718be0028592ffcc4824f0ccb
Rena Yamadera <renapi200i@i.softbank.jp>
2022-04-28 11:57:12 +0900

    企業の横並びできた

--- src/toppage/agent_list.php
+++ src/toppage/agent_list.php
@@ -1,4 +1,4 @@
-<div>
+<div class="agent-whole-box">
     <!--エージェント一覧コンテナ-->
     <?php 
     $all_agents=[1,2,3,4,5,6];//連想配列でエージェントの情報すべてが入る
@@ -8,9 +8,9 @@
 
     if($page_number-1!=floor(count($all_agents)/$agents_per_page)){//ページ番号が最後以外
         for ($i = 0; $i < $agents_per_page; $i++) {
-            echo '<div>';
+            echo '<div class="agent-overview-box">';
             echo '<div style="display:flex;">';
-            echo '<div><img src="../img/dummy.png" alt="エージェントの画像"></div>';
+            echo '<img class="agent-list-image" src="../img/dummy.png" alt="エージェントの画像">';
             echo '<div>';
             echo '<div>企業'.$all_agents[$i].'</div>';
             echo '<div>何が入るのか忘れたからとりあえずdiv</div>';
@@ -25,7 +25,7 @@
         for ($i = 0; $i < count($all_agents)%$agents_per_page; $i++) {//余りの個数出力
             echo '<div>';
             echo '<div style="display:flex;">';
-            echo '<div><img alt="エージェントの画像"></div>';
+            echo '<img class="agent-list-image" src="../img/dummy.png" alt="エージェントの画像">';
             echo '<div>';
             echo '<div>何が入るのか忘れたからとりあえずdiv</div>';
             echo '<div>何が入るのか忘れたからとりあえずdiv</div>';
