9c530f81134100e03bec6b1453752ce52a559d57
Yuta0227 <yutahonjo@keio.jp>
2022-04-28 19:27:09 +0900

    エージェント画面の特定日の申込一覧のjs修正した

--- src/agent/parts/agent_apply.php
+++ src/agent/parts/agent_apply.php
@@ -1,14 +1,21 @@
+<div><?php echo $_GET['year'].'年'.$_GET['month'].'月'.$_GET['date'].'日の申込一覧';?></div>
+<table>
+    <tr>
+        <th>申込日時</th>
+        <th>メールアドレス</th>
+    </tr>
+</table>
 <?php
 $applies_array = [
     ['10:50', 'sample@gmail.com'],
     ['10:10', 'sample@gmail.com']
     //パラメータの日程の申込一覧をデータベースから取得
 ];
-$index = 0;
-foreach ($applies_array as $apply) {
+for($index=0;$index<count($applies_array);$index++) {
+    ${"report_status".$index}=0;
     echo '<form method="POST" onsubmit="submitEvent();return false;" id="test' . $index . '" style="padding:10px;align-items:center;display:flex;border:1px solid black;">';
-    echo '<div>' . $apply[0] . '</div>';
-    echo '<div>' . $apply[1] . '</div>';
+    echo '<div>' . $applies_array[$index][0] . '</div>';
+    echo '<div>' . $applies_array[$index][1] . '</div>';
     echo '<input type="button" id="open_apply' . $index . '" value="詳細▽">';
     echo '<input id="close_apply' . $index . '" name="close' . $index . '" hidden value="閉じる△" type="submit">';
     echo '</form>';
@@ -22,13 +29,54 @@ foreach ($applies_array as $apply) {
     echo '</div>';
     echo '<form name="report_form'.$index.'" onsubmit="submit_reason();" action="" method="POST">';
     echo '<div style="justify-content:center;display:flex;border:1px solid black;">';
-    echo '<div id="new_report'.$index.'" hidden style="text-align:center;width:50%;padding:10px;border-radius:50%;background-color:red;">通報する('.($new_apply['月']+1).'月1日23:59まで)';
-    echo'</div>';
-    echo '<div id="new_reported'.$index.'" hidden style="text-align:center;width:50%;padding:10px;border-radius:50%;background-color:blue;">通報済み</div>';
+    if(${"report_status".$index}==0){
+        //通報していない場合
+        echo '<div id="report'.$index.'" hidden style="text-align:center;width:50%;padding:10px;border-radius:50%;background-color:red;">通報する('.($new_apply['月']+1).'月1日23:59まで)</div>';
+    }else{
+        //通報した場合
+        echo '<div id="reported'.$index.'" hidden style="text-align:center;width:50%;padding:10px;border-radius:50%;background-color:blue;">通報済み</div>';
+    }
     echo '</div>';
-    echo '<div id="new_report_reason'.$index.'" style="border:1px solid black;" hidden><div style="display:flex;justify-content:center;align-items:center;"><span>通報理由：</span><textarea type="text" name="new_report_reason" required placeholder="理由を記入してください"></textarea></div>';
-    echo '<div style="display:flex;justify-content:center;"><input type="submit" name="report'.$index.'"></div></div>';
+    echo '<div id="report_reason'.$index.'" style="border:1px solid black;" hidden><div style="display:flex;justify-content:center;align-items:center;"><span>通報理由：</span><textarea type="text" name="new_report_reason" required placeholder="理由を記入してください"></textarea></div>';
+    echo '<div style="display:flex;justify-content:center;"><div id="cancel_report'.$index.'">キャンセル</div><input type="submit" name="report'.$index.'"></div></div>';
     echo '</form>';
-    $index++;
 };
 echo '<div>' . $_GET['month'] . '月' . $_GET['date'] . '日の合計：' . count($applies_array) . '人</div>';
+?>
+<script>
+<?php for($index=0;$index<count($applies_array);$index++) {?>
+        document.getElementById('open_apply<?php echo $index; ?>').addEventListener('click', function() {
+            document.getElementById('close_apply<?php echo $index; ?>').removeAttribute('hidden');
+            document.getElementById('open_apply<?php echo $index; ?>').setAttribute('hidden', '');
+            document.getElementById('apply_detail<?php echo $index; ?>').removeAttribute('hidden');
+            <?php if( ${'report_status'.$index}==0){?>
+                document.getElementById('report<?php echo $index;?>').removeAttribute('hidden');
+            <?php }else{?>
+                document.getElementById('reported<?php echo $index;?>').removeAttribute('hidden');
+            <?php }?>
+        });
+        document.getElementById('close_apply<?php echo $index; ?>').addEventListener('click', function() {
+            document.getElementById('close_apply<?php echo $index; ?>').setAttribute('hidden', '');
+            document.getElementById('open_apply<?php echo $index; ?>').removeAttribute('hidden');
+            document.getElementById('apply_detail<?php echo $index; ?>').setAttribute('hidden', '');
+            document.getElementById('reported<?php echo $index;?>').setAttribute('hidden','');
+            document.getElementById('report<?php echo $index;?>').setAttribute('hidden','');
+            <?php if( ${'report_status'.$index}==0){?>
+                document.report_form<?php echo $index;?>.submit();
+            <?php }?>
+
+        });
+        document.getElementById('cancel_report<?php echo $index;?>').addEventListener('click',function(){
+            document.getElementById('report_reason<?php echo $index;?>').setAttribute('hidden','');
+            document.getElementById('report<?php echo $index;?>').removeAttribute('hidden','');
+        })
+        document.getElementById('report<?php echo $index;?>').addEventListener('click',function(){
+            document.getElementById('report<?php echo $index;?>').setAttribute('hidden','');
+            document.getElementById('report_reason<?php echo $index;?>').removeAttribute('hidden');
+        });
+        function submit_reason(){
+            document.getElementById('report_reason<?php echo $index;?>').setAttribute('hidden','');
+            document.getElementById('reported<?php echo $index;?>').removeAttribute('hidden');            
+        };
+    <?php }; ?>
+</script>
