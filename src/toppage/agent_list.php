99f8416b7ade78a4183495f725a1b0e3a279b886
Rena Yamadera <renapi200i@i.softbank.jp>
2022-04-30 16:10:45 +0900

    デザイン変更、リンク追加、ホバー時の動きつけた

--- src/toppage/agent_list.php
+++ src/toppage/agent_list.php
@@ -9,18 +9,23 @@
     if($page_number-1!=floor(count($all_agents)/$agents_per_page)){//ページ番号が最後以外
         for ($i = 0; $i < $agents_per_page; $i++) {
             echo '<div class="agent-overview-box">';
-            echo '<div style="display:flex;">';
-            echo '<img class="agent-list-image" src="../img/dummy.png" alt="エージェントの画像">';
+            echo '<a href="#" target="_blank" class="agent-overview-link">';
             echo '<div>';
             echo '<div class="agent-name">企業'.$all_agents[$i].'</div>';
-            echo '<div>#理系企業</div>';
-            echo '<div>#外資系企業</div>';
+            echo '<div>#理系企業#外資系企業</div>';
             echo '</div>';
+            echo '<div style="display: flex;">';
+            echo '<div class="image-box">';
+            echo '<img class="agent-list-image" src="../img/dummy.png" alt="エージェントの画像">';
             echo '</div>';
-            echo '<div style="text-align:center;">';
-            echo '<div class="agent-short-explanation">テキスト<br>説明文<br>文章の長さにもよるけど3~4行目安</div>';
+            echo '<div class="agent-article">';
+            echo '<div class="agent-short-explanation">各エージェントの概要説明を記入する。文章の長さにもよるけど3~4行目安</div>';
+            echo '<div style="text-align: center;">';
             echo '<div class="like-button">お気に入りに追加<i class="fa-regular fa-heart like-icon"></i></div>';
             echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</a>';
             echo '</div>';
         }; 
     }elseif($page_number-1==floor(count($all_agents)/$agents_per_page)){//ページ番号が最後の場合。数字にずれたぶんある。0か1。
