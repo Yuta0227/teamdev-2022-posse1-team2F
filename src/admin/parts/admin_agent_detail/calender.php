08b3f30d418b82e788a50b1a903128f648eeeec8
Yuta0227 <yutahonjo@keio.jp>
2022-05-01 18:40:51 +0900

    adminのカレンダーデータベースから引っ張ることに成功

--- src/admin/parts/admin_agent_detail/calender.php
+++ src/admin/parts/admin_agent_detail/calender.php
@@ -1,7 +1,7 @@
 <section>
-
     <div>
         <?php
+        //たぶんエラーの原因は月、日の0ついてるかどうか
         if (!isset($_GET['month'])) {
             echo date('n');
         } else {
@@ -16,69 +16,107 @@
         $calender_dates = []; //カレンダーで表示する日
         $year_month_parameter_set = isset($_GET['year']) && isset($_GET['month']);
         if ($year_month_parameter_set) {
-            if ($_GET['month'] != 1) {
+            if ($_GET['month'] != '01') {
                 //パラメータのmonthが1じゃないなら普通に月減らす
-                $decrease_month = $_GET['month'] - 1;
+                $decrease_month=$adjust->single($_GET['month'])-1;
+                $decrease_month=$adjust->double($decrease_month);
                 $decrease_year = $_GET['year'];
             } else {
-                $decrease_month = 12;
+                $decrease_month = '12';
                 $decrease_year = $_GET['year'] - 1;
             };
         }
         if ($year_month_parameter_set) {
-            if ($_GET['month'] != 12) {
+            if ($_GET['month'] != '12') {
                 //パラメータのmonthが12じゃないなら普通に月増やす
-                $increase_month = $_GET['month'] + 1;
+                $increase_month=$adjust->single($_GET['month'])+1;
+                $increase_month=$adjust->double($increase_month);
                 $increase_year = $_GET['year'];
             } else {
-                $increase_month = 1;
+                $increase_month = '01';
                 $increase_year = $_GET['year'] + 1;
             };
         }
         if ($year_month_parameter_set) {
             //パラメータセット時
             $year_month = $_GET['year'] . '-' . $_GET['month']; //パラメータの年月
-            $year_last_month = $_GET['year'] . '-' . ($_GET['month'] - 1); //パラメータの年月の先月
+            $year_last_month = $_GET['year'] . '-' . $decrease_month; //パラメータの年月の先月
             $date = date($year_month . '-' . date('t', strtotime($year_month))); //yearパラメータ-monthパラメータ-パラメータ年月の最終日
             $weeks_per_month = ceil((date('d', strtotime($date)) - date('w', strtotime($date)) - 1) / 7) + 1; //月における週の数をとる
             for ($day = date('w', strtotime($year_month . '-01')) - 1; $day >= 0; $day--) {
-                $number_of_applies += array(($decrease_month . '/' . date('t', strtotime($year_last_month)) - $day) => 3);
+                $decrease_date=date('t',strtotime($year_last_month))-$adjust->double($day);
+                $start_decrease_year_month_date=$decrease_year."-".$decrease_month."-".$decrease_date." 00:00:00";
+                $end_decrease_year_month_date=$decrease_year."-".$decrease_month."-".$decrease_date." 23:59:59";
+                $applies_per_day_stmt=$db->prepare("select count(apply_id) from apply_list where apply_id=? and apply_time between ? and ?;");
+                $applies_per_day_stmt->bindValue(1,$_GET['agent_index']);                
+                $applies_per_day_stmt->bindValue(2,$start_decrease_year_month_date);     
+                $applies_per_day_stmt->bindValue(3,$end_decrease_year_month_date);
+                $applies_per_day_stmt->execute();
+                $applies_per_day_data=$applies_per_day_stmt->fetchAll();                
+                $number_of_applies += array(($adjust->single($decrease_month) . '/' . $adjust->single(date('t', strtotime($year_last_month)) - $day)) => $applies_per_day_data);
                 //該当月の1日の曜日を数字で取得して先月の最後の方を配列に追加する
-                array_push($calender_dates, ($decrease_month . '/' . date('t', strtotime($year_last_month)) - $day));
+                array_push($calender_dates, ($adjust->single($decrease_month) . '/' . $adjust->single(date('t', strtotime($year_last_month)) - $day)));
                 //カレンダーで表示する日付を日付配列に追加
             }
             for ($day = 1; $day <= date('t', strtotime($year_month)); $day++) {
-                $number_of_applies += array($_GET['month'] . '/' . $day => 4);
+                $start_year_month_date=$_GET['year']."-".$_GET['month']."-".$adjust->double($day)." 00:00:00";
+                $end_year_month_date=$_GET['year']."-".$_GET['month']."-".$adjust->double($day)." 23:59:59";
+                $applies_per_day_stmt=$db->prepare("select count(apply_id) from apply_list where agent_id=? and apply_time between ? and ?;");
+                $applies_per_day_stmt->bindValue(1,$_GET['agent_index']);                
+                $applies_per_day_stmt->bindValue(2,$start_year_month_date);                
+                $applies_per_day_stmt->bindValue(3,$end_year_month_date);
+                $applies_per_day_stmt->execute();
+                $applies_per_day_data=$applies_per_day_stmt->fetchAll();  
+                $number_of_applies += array($adjust->single($_GET['month']) . '/' . $adjust->single($day) => $applies_per_day_data);
                 //4には申込件数代入
-                array_push($calender_dates, (($_GET['month'] . '/' . $day)));
+                array_push($calender_dates, (($adjust->single($_GET['month']) . '/' . $adjust->single($day))));
                 //カレンダーで表示する日付を日付配列に追加
             }
             for ($day = 1; $day <= 6 - date('w', strtotime($year_month . '-' . date('t', strtotime($year_month)))); $day++) { //来月の何日分出力するか今月の最終日の曜日をもとにきめる
-                array_push($calender_dates, ($increase_month . '/' . $day));
+                $start_increase_year_month_date=$increase_year."-".$increase_month."-".$adjust->double($day)." 00:00:00";
+                $end_increase_year_month_date=$increase_year."-".$increase_month."-".$adjust->double($day)." 23:59:59";
+                $applies_per_day_stmt=$db->prepare("select count(apply_id) from apply_list where agent_id=? and apply_time between ? and ?;");
+                $applies_per_day_stmt->bindValue(1,$_GET['agent_index']);                
+                $applies_per_day_stmt->bindValue(2,$start_increase_year_month_date);                
+                $applies_per_day_stmt->bindValue(3,$end_increase_year_month_date);
+                $applies_per_day_stmt->execute();
+                $applies_per_day_data=$applies_per_day_stmt->fetchAll();                
+                array_push($calender_dates, ($adjust->single($increase_month) . '/' . $adjust->single($day)));
                 //来月の日付を日付配列に追加
-                $number_of_applies += array(($increase_month . '/' . $day) => 6);
+                $number_of_applies += array(($adjust->single($increase_month) . '/' . $adjust->single($day)) => $applies_per_day_data);
             }
-        } else {
-            //パラメータ未セット時
-            $year_month = date('Y') . '-' . date('n');
-            $year_last_month = date('Y') . (date('n') - 1);
-            $last_month = date('n') - 1;
-            $date = date('Y-m-' . date('t')); //2022/04/30
-            $weeks_per_month = ceil((date('d', strtotime($date)) - date('w', strtotime($date)) - 1) / 7) + 1; //月における週の数をとる
-            //該当月の1日の曜日を数字で取得して先月の最後の方を配列に追加する
-            for ($i = date('w', strtotime($year_month . '-01')) - 1; $i >= 0; $i--) { //今月の一日の曜日をもとに先月の何日分出力するか
-                $number_of_applies += array(($last_month . '/' . date('t', strtotime($year_last_month)) - $i) => 3);
-                //先月の最後の件数を件数配列に追加
-                array_push($calender_dates, (($last_month . '/' . date('t', strtotime($year_last_month) - $i))));
-                //カレンダーで表示する日付を日付配列に追加
-            }
-            for ($day = 1; $day <= date('t'); $day++) {
-                $number_of_applies += array(date('n') . '/' . $day => 4);
-                //4には申込件数代入
-                array_push($calender_dates, ((date('n') . '/' . $day)));
-                //カレンダーで表示する日付を日付配列に追加
-            }
-        }
+        } 
+        // else {
+        //     //パラメータ未セット時
+        //     $year_month = date('Y') . '-' . date('n');
+        //     $year_last_month = date('Y') .'-'. (date('n') - 1);
+        //     $last_month = date('n') - 1;
+        //     $date = date('Y-m-' . date('t')); //2022/04/30
+        //     $weeks_per_month = ceil((date('d', strtotime($date)) - date('w', strtotime($date)) - 1) / 7) + 1; //月における週の数をとる
+        //     //該当月の1日の曜日を数字で取得して先月の最後の方を配列に追加する
+        //     for ($i = date('w', strtotime($year_month . '-01')) - 1; $i >= 0; $i--) { //今月の一日の曜日をもとに先月の何日分出力するか
+        //         $applies_per_day_stmt->bindValue(1,$_GET['agent_index']);                
+        //         $applies_per_day_stmt->bindValue(2,$year_last_month.'-'.date('t',strtotime($year_last_month)-$i));                
+        //         $applies_per_day_stmt->bindValue(3,$year_last_month.'-'.date('t',strtotime($year_last_month)-$i));
+        //         $applies_per_day_stmt->execute();
+        //         $applies_per_day_data=$applies_per_day_stmt->fetchAll();                
+        //         $number_of_applies += array(($last_month . '/' . date('t', strtotime($year_last_month)) - $i) => $applies_per_day_data);
+        //         //先月の最後の件数を件数配列に追加
+        //         array_push($calender_dates, (($last_month . '/' . date('t', strtotime($year_last_month) - $i))));
+        //         //カレンダーで表示する日付を日付配列に追加
+        //     }
+        //     for ($day = 1; $day <= date('t'); $day++) {
+        //         $applies_per_day_stmt->bindValue(1,$_GET['agent_index']);                
+        //         $applies_per_day_stmt->bindValue(2,$year_month.'-'.$day);                
+        //         $applies_per_day_stmt->bindValue(3,$year_month.'-'.$day);
+        //         $applies_per_day_stmt->execute();
+        //         $applies_per_day_data=$applies_per_day_stmt->fetchAll();                
+        //         $number_of_applies += array(date('n') . '/' . $day => $applies_per_day_data);
+        //         //4には申込件数代入
+        //         array_push($calender_dates, ((date('n') . '/' . $day)));
+        //         //カレンダーで表示する日付を日付配列に追加
+        //     }
+        // }
         for ($week = 1; $week <= $weeks_per_month; $week++) {
             echo '<tr>';
             for ($i = 1; $i <= 7; $i++) {
@@ -86,11 +124,11 @@
                 if ($year_month_parameter_set) {
                     //パラメータセット時
                     echo '<div id="date' . $i - 7 + 7 * $week . '" style="position:absolute;top:0;left:0;width:40%;height:40%;;text-align:center;">' . $calender_dates[$i - 7 + $week * 7 - 1] . '</div>';
-                    echo '<div id="number' . $i - 7 + 7 * $week . '" style="position:absolute;bottom:0;right:0;width:60%;height:60%;text-align:center;">' . $number_of_applies[$calender_dates[$i - 7 + $week * 7 - 1]] . '件</div>';
+                    echo '<div id="number' . $i - 7 + 7 * $week . '" style="position:absolute;bottom:0;right:0;width:60%;height:60%;text-align:center;">' . $number_of_applies[$calender_dates[$i - 7 + $week * 7 - 1]][0]['count(apply_id)'] . '件</div>';
                 } else {
                     //パラメータ未セット時
                     echo '<div id="date' . $i - 7 + 7 * $week . '" style="position:absolute;top:0;left:0;width:40%;height:40%;text-align:center;">' . $calender_dates[$i - 7 + $week * 7 - 1] . '</div>';
-                    echo '<div id="number' . $i - 7 + 7 * $week . '" style="position:absolute;bottom:0;right:0;width:60%;height:60%;text-align:center;">' . $number_of_applies[$calender_dates[$i - 7 + $week * 7 - 1]] . '件</div>';
+                    echo '<div id="number' . $i - 7 + 7 * $week . '" style="position:absolute;bottom:0;right:0;width:60%;height:60%;text-align:center;">' . $number_of_applies[$calender_dates[$i - 7 + $week * 7 - 1]][0]['count(apply_id)'] . '件</div>';
                 }
                 echo '</td>';
             };
@@ -111,7 +149,7 @@
                 $decrease_month = 12;
                 $decrease_year = $_GET['year'] - 1;
             };
-            echo $admin_agent_detail_url . '?year=' . $decrease_year . '&month=' . $decrease_month;
+            echo $admin_agent_detail_url . '?year=' . $decrease_year . '&month=' . $decrease_month.'&agent_index='.$_GET['agent_index'];
         } else {
             if (date('n') != 1) {
                 //パラメータのmonthが1じゃないなら普通に月減らす
@@ -121,7 +159,7 @@
                 $decrease_month = 12;
                 $decrease_year = date('Y') - 1;
             };
-            echo $admin_agent_detail_url . '?year=' . $decrease_year . '&month=' . $decrease_month;
+            echo $admin_agent_detail_url . '?year=' . $decrease_year . '&month=' . $decrease_month.'&agent_index='.$_GET['agent_index'];
         }
         ?>
         ">
@@ -153,7 +191,7 @@
                             $increase_month = 1;
                             $increase_year = $_GET['year'] + 1;
                         };
-                        echo $admin_agent_detail_url . '?year=' . $increase_year . '&month=' . $increase_month;
+                        echo $admin_agent_detail_url . '?year=' . $increase_year . '&month=' . $increase_month.'&agent_index='.$_GET['agent_index'];
                     }
 
         ?>">
