8482f55836f9d18704f1a0c5de95fc2041c89f48
Rena Yamadera <renapi200i@i.softbank.jp>
2022-05-07 16:07:46 +0900

    特集記事一覧画面にflexとかつけた

--- src/toppage/special_list.php
+++ src/toppage/special_list.php
@@ -1,7 +1,7 @@
 <?php
 $special_list_array = [
     [
-        'ハッシュタグ' => [1, 2, 3],
+        'ハッシュタグ' => ['理系', '外資系', '内定率が高い'],
         '投稿日' => '2022/02/27',
         'テキスト' => 'テキスト1',
         '画像' => '',
@@ -29,27 +29,29 @@ count($special_list_array) == $specials_per_page
 //この場合バグる。最後のページで表示できるものがないと発生
 
 ?>
-<div>
+<section class="special-list-section">
+<div class="special-list-all">
     <!--sectionに変えるべきかも-->
     <!-- php for文はif。一ページにのせる数で割り切れる場合はその数をすべてのページでfor。割り切れないときは最後のページのみ余りの数分出力 -->
     <?php for ($i = 0; $i < $specials_per_page; $i++) {
-        echo '<div>';
+        echo '<div class="special-list-each-box">';
         echo '<div>';
         echo '<img src="' . $special_list_array[($i + 1) * $specials_pagination - 1]['画像'] . '" alt="特集記事用の画像' . ($i + 1) * $specials_pagination . '">';
         echo '</div>';
-        echo '<div>';
-        echo '<div style="display:flex;">';
+        echo '<div class="special-list-informations">';
+        echo '<div class="special-list-information-tags">';
         foreach ($special_list_array[($i + 1) * $specials_pagination - 1]['ハッシュタグ'] as $data) {;
             echo '<div style="border:1px black solid;">#' . $data . '</div>';
         };
         echo '</div>';
-        echo '<div>';
-        echo $special_list_array[($i + 1) * $specials_pagination - 1]['投稿日'];
+        echo '<div class="special-list-information-explanation">';
+        echo $special_list_array[($i + 1) * $specials_pagination - 1]['テキスト'];
         echo '</div>';
         echo '<div>';
-        echo $special_list_array[($i + 1) * $specials_pagination - 1]['テキスト'];
+        echo $special_list_array[($i + 1) * $specials_pagination - 1]['投稿日'] . '投稿';
         echo '</div>';
         echo '</div>';
         echo '</div>';
     }; ?>
 </div>
+</section>
