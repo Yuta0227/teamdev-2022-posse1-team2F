930ffa507b03ee0a7fa7d10cf3feb294fbbb99e9
Rena Yamadera <renapi200i@i.softbank.jp>
2022-05-11 17:51:13 +0900

    ブランチ変更前のコミット、プッシュ

--- src/admin/parts/header.php
+++ src/admin/parts/header.php
@@ -1,7 +1,9 @@
 <header>
-    <img src="../img/boozer_logo.png" alt="logo">
-    <div>
-        <div>
+    <div class="admin-header-all">
+        <div class="admin-header-logo-box">
+        <img src="../../img/syukatudotcom_logo_white.png" alt="logo">
+        </div>
+        <div class="">
             <a href="index.php">
                 企業一覧
             </a>
