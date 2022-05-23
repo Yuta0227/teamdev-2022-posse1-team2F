92192c9031a8dd3489fcc11c0cae6228901eeaf1
Rena Yamadera <renapi200i@i.softbank.jp>
2022-05-23 17:29:42 +0900

    プライバシーポリシーCSS完了

--- src/toppage/pages/privacy_policy.php
+++ src/toppage/pages/privacy_policy.php
@@ -13,20 +13,20 @@
 </head>
 <body>
     <?php require "../parts/header.php";?>
-    <div>
+    <section class="privacy-policy-unit">
+    <div class="privacy-policy-all-box">
         <h1>プライバシーポリシー</h1>
         <p>Boozer（以下，「当社」といいます。）は，本ウェブサイト上で提供するサービス（以下,「本サービス」といいます。）における，ユーザーの個人情報の取扱いについて，以下のとおりプライバシーポリシー（以下，「本ポリシー」といいます。）を定めます。</p>
-    </div>
-    <div>
-        <h2>第1条（個人情報）</h2>
+    <div class="privacy-policy-each-section"> 
+        <h2 class="privacy-policy-each-section-head">第1条（個人情報）</h2>
         <p>「個人情報」とは，個人情報保護法にいう「個人情報」を指すものとし，生存する個人に関する情報であって，当該情報に含まれる氏名，生年月日，住所，電話番号，連絡先その他の記述等により特定の個人を識別できる情報及び容貌，指紋，声紋にかかるデータ，及び健康保険証の保険者番号などの当該情報単体から特定の個人を識別できる情報（個人識別情報）を指します。</p>
     </div>
-    <div>
-        <h2>第2条（個人情報の収集方法）</h2>
+    <div class="privacy-policy-each-section">
+        <h2 class="privacy-policy-each-section-head">第2条（個人情報の収集方法）</h2>
         <p>当社は，ユーザーが利用登録をする際に氏名，生年月日，住所，電話番号，メールアドレスなどの個人情報をお尋ねすることがあります。また，ユーザーと提携先などとの間でなされたユーザーの個人情報を含む取引記録や決済に関する情報を,当社の提携先（情報提供元，広告主，広告配信先などを含みます。以下，｢提携先｣といいます。）などから収集することがあります。</p>
     </div>
-    <div>
-        <h2>第3条（個人情報を収集・利用する目的）</h2>
+    <div class="privacy-policy-each-section">
+        <h2 class="privacy-policy-each-section-head">第3条（個人情報を収集・利用する目的）</h2>
         <p>当社が個人情報を収集・利用する目的は，以下のとおりです。</p>
         <ol>
             <li>当社サービスの提供・運営のため</li>
@@ -37,16 +37,16 @@
             <li>上記の利用目的に付随する目的</li>
         </ol>
     </div>
-    <div>
-        <h2>第4条（利用目的の変更）</h2>
+    <div class="privacy-policy-each-section">
+        <h2 class="privacy-policy-each-section-head">第4条（利用目的の変更）</h2>
         <ol>
             <li>当社は，利用目的が変更前と関連性を有すると合理的に認められる場合に限り，個人情報の利用目的を変更するものとします。</li>
             <li>利用目的の変更を行った場合には，変更後の目的について，当社所定の方法により，ユーザーに通知し，または本ウェブサイト上に公表するものとします。</li>
         </ol>
         <p></p>
     </div>
-
-    <p>テキスト</p>
+    </div>
+    </section>
     <?php require "../parts/footer.php";?>
 </body>
 </html>
