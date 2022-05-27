2151a7f9a8107a64804fffbc1948337841679a17
Your Name <you@example.com>
2022-05-27 17:58:18 +0900

    インジケータ微調整

--- src/toppage/parts/indicator_step3.php
+++ src/toppage/parts/indicator_step3.php
@@ -1,6 +1,6 @@
 <!-- コピペ -->
 <ul class="progressbar">
-    <li class="complete">STEP1 <br>企業を比較・問い合わせリストに追加</li>
+    <li class="complete">STEP1 <br>企業を比較・<br>リストに追加</li>
     <li class="complete">STEP2<br>リストを確認</li>
     <li class="complete">STEP3<br>情報入力</li>
     <li class="">STEP4<br>送信完了</li>
